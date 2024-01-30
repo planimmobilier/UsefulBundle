e564daa4a6f14af9054cdbcdb80f26b126b1606a
pasini <jeremy.pasini1@gmail.com>
2024-01-30 15:27:20 +0100

    [hotfix 5.0.7] clean

--- assets/js/useful-bootstrap.js
+++ assets/js/useful-bootstrap.js
@@ -3,29 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         new Autocomplete(el, {
             dataApiUrl: el.getAttribute('data-path'),
             maxRows: 30,
-            entityAlias: el.getAttribute('data-alias-useful'),
-            onSelectItem: (item) => {
-                document.querySelectorAll('.programme_id').forEach(el => el.value = item.value);
-                document.querySelectorAll('.utilisateur_id').forEach(el => el.value = item.value);
-            }
+            entityAlias: el.getAttribute('data-alias-useful')
         });
     });
-})
-/*
-jQuery(function () {
-    $('.autocomplete_input_useful').typeahead({
-        source: function (query, process) {
-            return $.get(this.$element.data('path'), {
-                maxRows: 30,
-                letters: query,
-                entity_alias: this.$element.data('aliasUseful')
-            }, function (data) {
-                return process($.parseJSON(data));
-            });
-        },
-        matcher: function (item) {
-            return true;
-        }
-    });
 });
- */
